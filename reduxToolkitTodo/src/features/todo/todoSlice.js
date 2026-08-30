410b798f1e04ccdcff55b6e4d1a73088a1846a78
Suhani Sabat <sabatsuhani74@gmail.com>
2026-08-30 23:46:21 +0530

    update todo added

--- reduxToolkitTodo/src/features/todo/todoSlice.js
+++ reduxToolkitTodo/src/features/todo/todoSlice.js
@@ -21,10 +21,19 @@ export const todoSlice = createSlice({
 
         removeTodo: (state, action) => {
             state.todos = state.todos.filter((todo) => todo.id != action.payload)
+        },
+
+        updateTodo: (state, action) => {
+            const { id, text } = action.payload
+            const todo = state.todos.find((todo) => todo.id === id)
+            if (todo) {
+                todo.text = text
+            }
         }
+
     }
 })
 
-export const {addTodo, removeTodo} = todoSlice.actions
+export const {addTodo, removeTodo, updateTodo} = todoSlice.actions
 
 export default todoSlice.reducer
