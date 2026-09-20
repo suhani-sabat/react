eb130eac6303321791117d8eb726e7c7e583b11b
Suhani Sabat <sabatsuhani74@gmail.com>
2026-09-20 09:03:08 +0530

    pages done

--- BlogWithAppwrite/src/components/index.js
+++ BlogWithAppwrite/src/components/index.js
@@ -1,8 +1,12 @@
-import Header from "./Header/Header"
-import Footer from "./Footer/Footer"
-import Container from "./Container/Container"
-import Logo from "./Logo"
-import LogoutBtn from "./Header/LogoutBtn"
-import RTE from './RTE'
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./Container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login"
+import PostForm from "./post-form/PostForm"
+import PostCard from "./PostCard";
 
-export { Header, Footer, Container, Logo, LogoutBtn, RTE }
+export { Header, Footer, Container, Logo, LogoutBtn, RTE, Signup, Login, PostForm, PostCard };
---
+++ BlogWithAppwrite/src/pages/AllPosts.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+import service from "../appwrite/service";
+import { Container, PostCard } from "../components";
+
+export default function AllPosts() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    service.getPost([]).then((posts) => {
+      if (posts) {
+        setPosts(posts.documents);
+      }
+    });
+  }, []);
+
+  return (
+    <div className="w-full py-8">
+      <Container>
+        <div className="flex flex-wrap">
+          {posts.map((post) => (
+            <div key={post.$id} className="p-2 w-1/4">
+              <PostCard {...post} />
+            </div>
+          ))}
+        </div>
+      </Container>
+    </div>
+  );
+}
